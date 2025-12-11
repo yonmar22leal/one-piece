ec118ad52816a757ecff13da13e412694db55f6c
Yonmar <yonmar22leal@gmail.com>
2025-12-11 19:34:49 -0400

    1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/one-piece/',
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-})
+  base: command === 'build' ? '/one-piece/' : '/',  // ✅ dev: '/', build: '/one-piece/'
+}))
